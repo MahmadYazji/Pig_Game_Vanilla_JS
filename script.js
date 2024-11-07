426c23653e3b390fc32e96126b158332f99b7deb
MohammedYazji <mahmadyazji@gmail.com>
2024-11-07 19:34:27 +0200

    Edit The Condition 100 insted 10 to win the Game

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ btnHold.addEventListener('click', function () {
       scores[activePlayer];
     // check if player's score is >= 100
     // Finish The Game
-    if (scores[activePlayer] >= 10) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       document
         .querySelector(`.player--${activePlayer}`)
